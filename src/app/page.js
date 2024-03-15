558f3d6ea6dec2109e5f52e2ef3f69b57572af22
Will Morgan <sghp78@durham.ac.uk>
2024-03-15 14:21:14 +0000

    stash

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,20 @@
+import {Col, Container, Row} from 'react-bootstrap';
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-function AutoLayoutSizingExample() {
+function AutoLayoutExample() {
   return (
     <Container>
       <Row>
-        <Col>1 of 3</Col>
-        <Col xs={6}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
       </Row>
       <Row>
         <Col>1 of 3</Col>
-        <Col xs={5}>2 of 3 (wider)</Col>
+        <Col>2 of 3</Col>
         <Col>3 of 3</Col>
       </Row>
     </Container>
   );
 }
 
-export default AutoLayoutSizingExample;
+export default AutoLayoutExample;
