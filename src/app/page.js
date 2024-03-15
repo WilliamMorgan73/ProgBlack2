056bf20405e2ce63a665386286127ed9df1e0bcc
mcrawf9 <milliecrawford05@gmail.com>
2024-03-15 13:23:04 +0000

    adding bootstrap to project

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
   return (
     <main>
+      <div class="col-md-4">
+        testing this out
+      </div>
 
     </main>
   );
