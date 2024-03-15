3b7472fa185bc11b1bf5d471d97c91d38b7b7bf5
Will Morgan <sghp78@durham.ac.uk>
2024-03-15 14:43:01 +0000

    Template sorted, need to add backend and containers for posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,21 @@
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MainColumn() {
   return (
-    <Container>
+    <Container fluid className='bg-dark' style={{ minHeight: '100vh' }}>
       <Row>
-        <Col  className="bg-dark"></Col>
-        <Col >hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</Col>
-        <Col  className="bg-dark"></Col>
+        <Col xs={2}></Col>
+        <Col xs={8} className="bg-light text-center">
+          <Row>
+            <h1>Post 1</h1>
+          </Row>
+          <Row>
+            <h1>Post 2</h1>
+          </Row>
+        </Col>
+        <Col xs={2}></Col>
       </Row>
     </Container>
   );
