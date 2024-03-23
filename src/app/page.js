0c77c74652de50d216432580ecd3750fe560b20f
mcrawf9 <milliecrawford05@gmail.com>
2024-03-23 13:00:44 +0000

    displaying posts from the json file

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './data.json';
 
 function MainColumn() {
   return (
@@ -8,16 +9,37 @@ function MainColumn() {
       <Row>
         <Col xs={2}></Col>
         <Col xs={8} className="bg-light text-center">
+          <h1>Anonymous Posts</h1>
           <Row>
-            <h1>Post 1</h1>
+            <hr></hr>
           </Row>
           <Row>
-            <h1>Post 2</h1>
+            {data.posts.map((posts) => (
+              <div key={posts.id}>
+                <p>{posts.content}</p>
+                <p>{posts.date}</p>
+                <hr></hr>
+              </div>
+            ))}
           </Row>
         </Col>
         <Col xs={2}></Col>
       </Row>
     </Container>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Users</h1>
+      {data.users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
