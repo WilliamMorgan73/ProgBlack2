791bac68ec33d41befbb0707d6a5321a32996fd9
mcrawf9 <milliecrawford05@gmail.com>
2024-03-23 14:57:06 +0000

    Adding posts modal appears #5

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
+"use client";
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Modal, Button, Form} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data.json';
-import styles from './styles.css'
+import styles from './styles.css';
+import { useState } from 'react';
+
 
 function MainColumn() {
+
+  const [appear, setAppear] = useState(false);
+  const handleExit = () => setAppear(false);
+  const handleAppear = () => setAppear(true);
+
   return (
     <Container fluid className='bg-dark' style={{ minHeight: '100vh' }}>
       <Row>
@@ -27,7 +35,32 @@ function MainColumn() {
         <Col xs={2}>
           <Row>
             <div className="buttonContainer">
-              <button type="button" className="btn position-absolute bottom-0 end-0" style={{ backgroundColor: '#68246D', color: 'white' }}>Add Post</button>
+              <button type="button" className="btn position-absolute bottom-0 end-0" style={{ backgroundColor: '#68246D', color: 'white' }} onClick={handleAppear} >Add Post</button>
+              <Modal show={appear} onHide={handleExit}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Add An Anonymous Post</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form>
+                  <Form.Group className="mb-3" controlId="addPostForm.ControlInput1">
+                    <Form.Label>Post Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      placeholder="dd/mm/yy"
+                      autoFocus
+                    />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="addPostForm.ControlTextarea1">
+                      <Form.Label>Post Content</Form.Label>
+                      <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                  </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleExit}>Close</Button>
+                  <Button variant="primary" style={{ backgroundColor: '#68246D', color: 'white' }} >Post</Button>
+                </Modal.Footer>
+              </Modal>
             </div>
           </Row>
         </Col>
