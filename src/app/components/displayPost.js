0f94b9600e3393800148021cc59985797084c40f
mcrawf9 <milliecrawford05@gmail.com>
2024-03-24 17:02:45 +0000

    posts show data and time

--- src/app/components/displayPost.js
+++ src/app/components/displayPost.js
@@ -7,7 +7,7 @@ const DisplayPost = () => {
                 {data.map((post) => (
                   <div key={post.postID}>
                     <p>{post.message}</p>
-                    <p>{post.creationTime}</p>
+                    <p>{post.creationTime.slice(0,10)} {post.creationTime.slice(11,16)}</p>
                     <hr></hr>
                   </div>
                 ))}
