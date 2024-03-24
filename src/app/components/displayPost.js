518e6a6652776cd3b404a500fef6dd0ca6d07dcb
mcrawf9 <milliecrawford05@gmail.com>
2024-03-24 17:09:02 +0000

    displaying post headings

--- src/app/components/displayPost.js
+++ src/app/components/displayPost.js
@@ -6,6 +6,7 @@ const DisplayPost = () => {
             <>
                 {data.map((post) => (
                   <div key={post.postID}>
+                    <h4>Post {post.postID}</h4>
                     <p>{post.message}</p>
                     <p>{post.creationTime.slice(0,10)} {post.creationTime.slice(11,16)}</p>
                     <hr></hr>
