3f79c2465a55eb20a903e5fde041d0d179976428
Will Morgan <sghp78@durham.ac.uk>
2024-03-24 17:25:03 +0000

    Changed how data is displayed

--- src/app/components/displayPost.js
+++ src/app/components/displayPost.js
@@ -1,19 +1,20 @@
+import { Card, Container } from 'react-bootstrap';
 const data = require('@/../data/data.json');
 
 const DisplayPost = () => {
-
-        return (
-            <>
-                {data.map((post) => (
-                  <div key={post.postID}>
-                    <h4>Post {post.postID}</h4>
-                    <p>{post.message}</p>
-                    <p>{post.creationTime.slice(0,10)} {post.creationTime.slice(11,16)}</p>
-                    <hr></hr>
-                  </div>
-                ))}
-            </>
-        ) 
-    }
+    return (
+        <Container style={{ maxWidth: '98%' }}>
+            {data.map((post) => (
+                <Card key={post.postID} style={{ border: '1px solid #ccc', borderRadius: '30px', margin: '10px 0' }}>
+                    <Card.Body>
+                        <Card.Title>Post {post.postID}</Card.Title>
+                        <Card.Text>{post.message}</Card.Text>
+                        <Card.Text>{post.creationTime.slice(0,10)} {post.creationTime.slice(11,16)}</Card.Text>
+                    </Card.Body>
+                </Card>
+            ))}
+        </Container>
+    );
+}
 
 export default DisplayPost;
